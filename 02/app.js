67e48c6734b71d0a23ab7d3ab79d3952d806771d
Karol Grzybowski <k.grzybowski11@gmail.com>
2024-05-30 15:46:34 +0200

    Task 3 finished

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,14 @@
 const x = 5;
 let result = 0;
 
-for (let i = 1; i <= x; i++){
+// for (let i = 1; i <= x; i++){
+//     result = result + i;
+//     console.log(result);
+// }
+
+let i = 1;
+while (i <= x) {
     result = result + i;
-    console.log(result);
+    i++;
 }
+console.log(result);
